5dee439dc1c66af88377d8f4dc28fe4d5bbe1e7b
Kevin Parra <53342165+Kevinparra535@users.noreply.github.com>
2020-09-07 17:51:21 -0500

    Todo listox2

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="flex-col items-center">
+  <div class="flex-col items-center text-center">
+    <lottie-player
+      src="https://assets7.lottiefiles.com/packages/lf20_CiKDQB.json"
+      background="transparent"
+      speed="1"
+      style="width: 100%; height: 300px;"
+      loop
+      autoplay
+    ></lottie-player>
     <h1 class="text-gray-700 text-6xl uppercase">error</h1>
-    <router-link class="mt-5 text-xl text-green-600 hover:underline" to="/"
+    <router-link
+      class="mt-5 text-xl bg-purple-500 py-2 px-4 text-gray-100 hover:underline rounded-lg"
+      to="/"
       >Volver a la página de Inicio</router-link
     >
   </div>
